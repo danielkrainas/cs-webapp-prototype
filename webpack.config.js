fabed0b9ddb35de8e0164965d0ca8ad9bac9b42e
Doug Boutwell <doug@cloudspot.io>
2017-08-09 10:30:39 -0700

    Generate index.html dynamically with webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -8,7 +9,18 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js',
   },
+  plugins: [new HtmlWebpackPlugin({
+    name: 'CloudSpot',
+    template: 'index.ejs',
+  })],
   module: {
+    loaders: [
+      {
+        test: /\.(html)$/,
+        loader: 'file-loader?name=[path][name].[ext]!./index.html',
+      },
+    ],
+
     rules: [
       {
         test: /\.vue$/,
