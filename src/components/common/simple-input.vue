ad36fa9479d015172b2b1e8b66aedd0552170e1a
Doug Boutwell <doug@cloudspot.io>
2017-08-10 17:59:25 -0700

    Simple-input accept an optional icon

--- src/components/common/simple-input.vue
+++ src/components/common/simple-input.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="simple-input top">
     <label v-if="this.label">{{ this.label }}</label>
-    <input name="gallery-url" :placeholder="myPlaceholder"></input>
+    <div class="input-container">
+      <span v-if="hasIcon" :class="myIconClass" class="fa input-icon"></span>
+      <input :class="hasIcon" name="gallery-url" :placeholder="myPlaceholder"></input>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'simple-input',
-  props: ['label', 'placeholder'],
+  props: ['label', 'placeholder', 'icon'],
   data () {
     return {
       myLabel: this.label,
       myPlaceholder: this.placeholder,
+      myIconClass: this.icon ? 'fa-' + this.icon : '',
+      hasIcon: this.icon ? 'has-icon' : '',
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../style/colors';
+
 .simple-input {
+  margin: 15px 0;
   display: flex;
 
   input {
@@ -28,6 +36,25 @@ export default {
     padding: 10px;
     width: 100%;
     flex-grow: 1;
+
+    &.has-icon {
+      text-indent: 27px;
+    }
   }
+
+  .input-container {
+    position: relative;
+    flex-grow: 1;
+    display: flex;
+  }
+
+  .input-icon {
+    position: absolute;
+    top: 13px;
+    left: 13px;
+    z-index: 10;
+    color: $color-accent;
+  }
+
 }
 </style>
