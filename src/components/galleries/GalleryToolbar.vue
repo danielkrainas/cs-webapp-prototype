027945637e250c2e1da09a0263e008c2f0f73910
Doug Boutwell <doug@cloudspot.io>
2017-08-11 14:24:31 -0700

    Add callout triangle to top menu bar

--- src/components/galleries/GalleryToolbar.vue
+++ src/components/galleries/GalleryToolbar.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="toolbar-section right">
       <ol class="nav-items">
-        <li v-for="item in navItems">
+        <li v-for="item in navItems" class="nav-item" :class="itemExtraClass(item)">
           <router-link :to="{ name: item.routeName }" class="button">
             {{ item.name }}
           </router-link>
@@ -20,8 +20,23 @@
 <script>
 export default {
   props: ['galleryId'],
+  watch: {
+    '$route': function () {
+      for (var i in this.navItems) {
+        if (this.$route.name === this.navItems[i].routeName) {
+          this.lastRouteName = this.$route.name
+        }
+      }
+    },
+  },
+  methods: {
+    itemExtraClass (item) {
+      return item.routeName === this.lastRouteName ? 'callout' : ''
+    },
+  },
   data () {
     return {
+      lastRouteName: this.$route.name,
       navItems: [{
         name: 'Images',
         routeName: 'galleryImages',
@@ -60,13 +75,52 @@ export default {
   }
 
   &.right {
+    height: 100%;
     justify-content: flex-end;
+    align-items: stretch;
   }
 }
 
 .nav-items {
+  align-items: stretch;
   display: flex;
 }
+
+.nav-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+.callout {
+  &::after,
+  &::before {
+    bottom: -1px;
+    left: 50%;
+    border: solid transparent;
+    content: ' ';
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+  }
+
+  &::after {
+    border-color: transparent;
+    border-bottom-color: $color-app-bg;
+    border-width: 8px;
+    margin-left: -8px;
+  }
+
+  &::before {
+    border-color: transparent;
+    border-bottom-color: $color-accent;
+    border-width: 10px;
+    margin-left: -10px;
+  }
+}
+
 
 .gallery-name {
   font-size: 18px;
